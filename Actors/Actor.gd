71e0badabdcca86c6d39a730d9dc2ee644b59d1c
Dylan <glsndylan@gmail.com>
2022-07-15 22:00:05 -0400

    actor base

--- Actors/Actor.gd
+++ Actors/Actor.gd
@@ -3,15 +3,22 @@ class_name Actor
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var gravity = 300.0
+export var speed = Vector2(300.0, 300.0)
+
+var velocity = Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _physics_process(delta: float) -> void:
+	# Runs physics every frame, based on seconds
+	velocity.y += gravity * delta
+	move_and_slide(velocity)
+	
