7c60b8a120416e9165a381ce6445085316662f1c
vvvalki <chokowrites@gmail.com>
2022-07-15 23:12:17 -0400

    AutoLoad
    check World.gd

---
+++ World.gd
@@ -0,0 +1,12 @@
+extends Node
+
+## WORLD VARIABLES ## 
+var move
+
+
+## you can call any of these variables in any scene:
+## i.e "GlobalWorld.move" would access the var move in this script
+
+
+
+
